a5960ec00728d864914433c507e21fca3babbf1a
Leonardo Cappuccio <leo@systemexception.org>
2016-07-01 23:21:02 +0200

    add application launcher

---
+++ src/main/java/org/systemexception/ecommuter/Application.java
@@ -0,0 +1,16 @@
+package org.systemexception.ecommuter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author leo
+ * @date 01/07/16 23:18
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}

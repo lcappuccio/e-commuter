ee9da982756d6e94ee433ae4848dfd24a608c8af
Leonardo Cappuccio <leo@systemexception.org>
2016-07-01 23:58:49 +0200

    add field

--- src/main/java/org/systemexception/ecommuter/Application.java
+++ src/main/java/org/systemexception/ecommuter/Application.java
@@ -1,5 +1,6 @@
 package org.systemexception.ecommuter;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class Application {
+
+	@Value("${google.maps.api.key}")
+	private String googleMapsApiKey;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);

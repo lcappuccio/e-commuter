cb2564791914f641f613cc66f8cf460982cba03b
Leonardo Cappuccio <leo@systemexception.org>
2016-07-02 17:22:57 +0200

    add equals and hashCode

--- src/main/java/org/systemexception/ecommuter/model/Address.java
+++ src/main/java/org/systemexception/ecommuter/model/Address.java
@@ -93,4 +93,49 @@ public class Address {
 	public void setPostalCode(String postalCode) {
 		this.postalCode = postalCode;
 	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		Address address = (Address) o;
+
+		if (Double.compare(address.latitude, latitude) != 0) return false;
+		if (Double.compare(address.longitude, longitude) != 0) return false;
+		if (streetNumber != null ? !streetNumber.equals(address.streetNumber) : address.streetNumber != null)
+			return false;
+		if (route != null ? !route.equals(address.route) : address.route != null) return false;
+		if (locality != null ? !locality.equals(address.locality) : address.locality != null) return false;
+		if (administrativeAreaLevel2 != null ? !administrativeAreaLevel2.equals(address.administrativeAreaLevel2) :
+				address.administrativeAreaLevel2 != null)
+			return false;
+		if (administrativeAreaLevel1 != null ? !administrativeAreaLevel1.equals(address.administrativeAreaLevel1) :
+				address.administrativeAreaLevel1 != null)
+			return false;
+		if (country != null ? !country.equals(address.country) : address.country != null) return false;
+		if (postalCode != null ? !postalCode.equals(address.postalCode) : address.postalCode != null) return false;
+		return formattedAddress != null ? formattedAddress.equals(address.formattedAddress) : address.formattedAddress
+				== null;
+
+	}
+
+	@Override
+	public int hashCode() {
+		int result;
+		long temp;
+		result = streetNumber != null ? streetNumber.hashCode() : 0;
+		result = 31 * result + (route != null ? route.hashCode() : 0);
+		result = 31 * result + (locality != null ? locality.hashCode() : 0);
+		result = 31 * result + (administrativeAreaLevel2 != null ? administrativeAreaLevel2.hashCode() : 0);
+		result = 31 * result + (administrativeAreaLevel1 != null ? administrativeAreaLevel1.hashCode() : 0);
+		result = 31 * result + (country != null ? country.hashCode() : 0);
+		result = 31 * result + (postalCode != null ? postalCode.hashCode() : 0);
+		result = 31 * result + (formattedAddress != null ? formattedAddress.hashCode() : 0);
+		temp = Double.doubleToLongBits(latitude);
+		result = 31 * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(longitude);
+		result = 31 * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
 }
--- src/main/java/org/systemexception/ecommuter/model/Person.java
+++ src/main/java/org/systemexception/ecommuter/model/Person.java
@@ -9,6 +9,8 @@ public class Person {
 	private String name, surname;
 	private Address address;
 
+	public Person() {}
+
 	public Person (final String name, final String surname, final Address address) {
 		this.name = name;
 		this.surname = surname;
@@ -37,5 +39,26 @@ public class Person {
 
 	public void setAddress(Address address) {
 		this.address = address;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		Person person = (Person) o;
+
+		if (name != null ? !name.equals(person.name) : person.name != null) return false;
+		if (surname != null ? !surname.equals(person.surname) : person.surname != null) return false;
+		return address != null ? address.equals(person.address) : person.address == null;
+
+	}
+
+	@Override
+	public int hashCode() {
+		int result = name != null ? name.hashCode() : 0;
+		result = 31 * result + (surname != null ? surname.hashCode() : 0);
+		result = 31 * result + (address != null ? address.hashCode() : 0);
+		return result;
 	}
 }

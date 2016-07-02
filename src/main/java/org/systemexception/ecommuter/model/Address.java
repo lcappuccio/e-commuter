40951bbd1a4cfea15157e10a7081eee69042588e
Leonardo Cappuccio <leo@systemexception.org>
2016-07-02 10:55:56 +0200

    add geo coordinates to address

--- src/main/java/org/systemexception/ecommuter/model/Address.java
+++ src/main/java/org/systemexception/ecommuter/model/Address.java
@@ -12,6 +12,25 @@ public class Address {
 	 */
 	private String streetNumber, route, locality, administrativeAreaLevel2, administrativeAreaLevel1, country,
 			postalCode;
+	private double latitude;
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	private double longitude;
 
 	public String getStreetNumber() {
 		return streetNumber;

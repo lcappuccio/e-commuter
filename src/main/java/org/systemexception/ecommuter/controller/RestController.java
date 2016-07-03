0b944b2c13adad0e65107c0b742c3fae379a8835
Leonardo Cappuccio <leo@systemexception.org>
2016-07-03 22:33:47 +0200

    add controller

---
+++ src/main/java/org/systemexception/ecommuter/controller/RestController.java
@@ -0,0 +1,49 @@
+package org.systemexception.ecommuter.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.systemexception.ecommuter.api.DatabaseApi;
+import org.systemexception.ecommuter.api.LocationApi;
+import org.systemexception.ecommuter.enums.Endpoints;
+import org.systemexception.ecommuter.exceptions.CsvParserException;
+import org.systemexception.ecommuter.exceptions.TerritoriesException;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author leo
+ * @date 03/07/16 22:06
+ */
+@Controller
+@RequestMapping(value = Endpoints.CONTEXT)
+public class RestController {
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	private DatabaseApi databaseApi;
+	@Autowired
+	private LocationApi locationService;
+
+	@RequestMapping(value = Endpoints.ADD_TERRITORIES, method = RequestMethod.POST,
+			produces = MediaType.TEXT_PLAIN_VALUE)
+	public ResponseEntity<HttpStatus> save(@RequestParam(Endpoints.FILE_TO_UPLOAD) final MultipartFile dataFile)
+			throws IOException, CsvParserException, TerritoriesException {
+
+		File savedDataFile = new File(dataFile.getOriginalFilename());
+		dataFile.transferTo(savedDataFile);
+		databaseApi.addTerritories(savedDataFile.getAbsolutePath());
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+}
---
+++ src/main/java/org/systemexception/ecommuter/enums/Endpoints.java
@@ -0,0 +1,14 @@
+package org.systemexception.ecommuter.enums;
+
+/**
+ * @author leo
+ * @date 03/07/16 22:04
+ */
+public class Endpoints {
+
+	public static final String CONTEXT = "/ecommuter";
+	public static final String ADD_TERRITORIES = "/addterritories";
+
+	public static final String ID_WITH_EXTENSION = "/{id:.+}";
+	public static final String FILE_TO_UPLOAD = "fileToUpload";
+}

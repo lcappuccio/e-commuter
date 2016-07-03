cb382ff6c2c163ec89f009542c06aea2fa1a24a8
Leonardo Cappuccio <leo@systemexception.org>
2016-07-03 21:24:51 +0200

    green: end2End

--- src/test/java/org/systemexception/ecommuter/test/End2End.java
+++ src/test/java/org/systemexception/ecommuter/test/End2End.java
@@ -14,12 +14,16 @@ import org.systemexception.ecommuter.api.LocationApi;
 import org.systemexception.ecommuter.exceptions.CsvParserException;
 import org.systemexception.ecommuter.exceptions.LocationException;
 import org.systemexception.ecommuter.exceptions.TerritoriesException;
+import org.systemexception.ecommuter.model.Address;
+import org.systemexception.ecommuter.model.Person;
+import org.systemexception.ecommuter.model.Persons;
 
 import java.io.File;
 import java.net.URISyntaxException;
 import java.net.URL;
 
 import static junit.framework.TestCase.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author leo
@@ -32,7 +36,7 @@ import static junit.framework.TestCase.assertNotNull;
 public class End2End {
 
 	@Autowired
-	private DatabaseApi sut;
+	private DatabaseApi databaseApi;
 	@Autowired
 	private LocationApi locationService;
 
@@ -40,11 +44,47 @@ public class End2End {
 	public void setUp() throws CsvParserException, TerritoriesException, URISyntaxException, LocationException {
 		URL myTestURL = ClassLoader.getSystemResource("it_data_SMALL.csv");
 		File myFile = new File(myTestURL.toURI());
-		sut.addTerritories(myFile.getAbsolutePath());
+		databaseApi.addTerritories(myFile.getAbsolutePath());
 	}
 
 	@Test
 	public void api_key_is_not_null() {
 		assertNotNull(Application.apiKey);
+	}
+
+	@Test
+	public void end2End() throws LocationException {
+		Person personA = new Person();
+		Address addressWorkA = locationService.geoToAddress(46.003509, 8.742917);
+		Address addressHomeA = locationService.geoToAddress(46.000490, 8.738347);
+		personA.setName("TEST_NAME_A");
+		personA.setSurname("TEST_SURNAME_A");
+		personA.setHomeAddress(addressHomeA);
+		personA.setWorkAddress(addressWorkA);
+
+		Person personB = new Person();
+		Address addressWorkB = locationService.geoToAddress(46.002834, 8.742499);
+		Address addressHomeB = locationService.geoToAddress(45.999950, 8.740594);
+		personB.setName("TEST_NAME_B");
+		personB.setSurname("TEST_SURNAME_B");
+		personB.setHomeAddress(addressHomeB);
+		personB.setWorkAddress(addressWorkB);
+
+		Person personC = new Person();
+		Address addressWorkC = locationService.geoToAddress(45.996015, 8.732703);
+		Address addressHomeC = locationService.geoToAddress(45.999659, 8.737842);
+		personC.setName("TEST_NAME_C");
+		personC.setSurname("TEST_SURNAME_C");
+		personC.setHomeAddress(addressHomeC);
+		personC.setWorkAddress(addressWorkC);
+
+		Persons persons = new Persons();
+		persons.addPerson(personA);
+		persons.addPerson(personB);
+		persons.addPerson(personC);
+
+		Persons nearbyPersons = locationService.findNearbyPersons(personA, persons, 0.5);
+
+		assertTrue(nearbyPersons.getPersons().size() == 1);
 	}
 }

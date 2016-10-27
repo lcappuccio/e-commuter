bba2c90630f37d6d2bcc6d48586aaabd116e80d4
Leonardo Cappuccio <leonardo.cappuccio@appway.com>
2016-10-27 11:34:45 +0200

    extend rest controller

--- src/main/java/org/systemexception/ecommuter/enums/Endpoints.java
+++ src/main/java/org/systemexception/ecommuter/enums/Endpoints.java
@@ -11,16 +11,18 @@ public class Endpoints {
 	// PERSON
 	public static final String PERSON = "/person";
 	public static final String PERSON_ADD = "/add";
+	public static final String PERSON_BY_LASTNAME = "/lastname";
 	public static final String PERSON_DELETE = "/delete";
-	public static final String PERSON_UPDATE = "/update";
 	public static final String PERSON_NEARBY = "/nearby";
+	public static final String PERSON_UPDATE = "/update";
 	// ADDRESS
 	public static final String ADDRESS = "/address";
 	public static final String GEO_TO_ADDRESS = "/fromgeo";
 	public static final String ADDRESS_TO_GEO = "/togeo";
 	// PARAMETERS
+	public static final String DISTANCE = "distance";
 	public static final String FILE_TO_UPLOAD = "fileToUpload";
+	public static final String LAST_NAME = "lastname";
 	public static final String LATITUDE = "latitude";
 	public static final String LONGITUDE = "longitude";
-	public static final String DISTANCE = "distance";
 }

ff8fc3cd1fbbf4a80602697e72b7f2a50cbf774b
Leo <leo@systemexception.org>
2017-08-05 09:48:13 +0200

    extract territories constructor

--- src/main/java/org/systemexception/ecommuter/model/Territories.java
+++ src/main/java/org/systemexception/ecommuter/model/Territories.java
@@ -1,6 +1,17 @@
 package org.systemexception.ecommuter.model;
 
+import org.apache.commons.csv.CSVRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.systemexception.ecommuter.enums.Constants;
+import org.systemexception.ecommuter.enums.CsvHeaders;
+import org.systemexception.ecommuter.exceptions.CsvParserException;
+import org.systemexception.ecommuter.pojo.CsvParser;
+
+import java.io.File;
+import java.security.InvalidParameterException;
 import java.util.HashSet;
+import java.util.List;
 
 /**
  * @author leo
@@ -8,17 +19,47 @@ import java.util.HashSet;
  */
 public class Territories {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(Territories.class);
+
 	private final HashSet<Territory> territories;
 
 	public Territories() {
-		territories = new HashSet<>();
+		this.territories = new HashSet<>();
 	}
 
-	public void addTerritory(Territory territory) {
-		territories.add(territory);
+	public Territories(File territoriesFile) throws CsvParserException {
+
+		territories = new HashSet<>();
+
+		LOGGER.info("readCsvTerritories" + Constants.LOG_OBJECT_SEPARATOR + territoriesFile.getName());
+		CsvParser csvParser = new CsvParser(territoriesFile);
+		List<CSVRecord> csvRecords = csvParser.readCsvContents();
+		for (CSVRecord csvRecord : csvRecords) {
+			String country = csvRecord.get(CsvHeaders.COUNTRY);
+			String postalCode = csvRecord.get(CsvHeaders.POSTAL_CODE);
+			String placeName = csvRecord.get(CsvHeaders.PLACE_NAME);
+			Territory territory = new Territory(country, postalCode, placeName);
+			territories.add(territory);
+		}
+		LOGGER.info("finishCsvTerritories" + Constants.LOG_OBJECT_SEPARATOR + territoriesFile.getName());
+
 	}
 
 	public HashSet<Territory> getTerritories() {
 		return territories;
+	}
+
+	private void isValidTerritory(final Territory territory) {
+		if (territories.contains(territory)) {
+			String errorMessage = "isValidTerritory"  + Constants.LOG_OBJECT_SEPARATOR + territory.getCountry() +
+					Constants.LOG_ITEM_SEPARATOR + territory.getPostalCode();
+			LOGGER.error(errorMessage);
+			throw new InvalidParameterException(errorMessage);
+		}
+	}
+
+	public void addTerritory(final Territory territory) {
+		isValidTerritory(territory);
+		territories.add(territory);
 	}
 }
--- src/test/java/org/systemexception/ecommuter/test/model/TerritoriesTest.java
+++ src/test/java/org/systemexception/ecommuter/test/model/TerritoriesTest.java
@@ -1,10 +1,17 @@
 package org.systemexception.ecommuter.test.model;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.neo4j.io.fs.FileUtils;
+import org.systemexception.ecommuter.enums.Constants;
+import org.systemexception.ecommuter.exceptions.CsvParserException;
 import org.systemexception.ecommuter.exceptions.TerritoriesException;
 import org.systemexception.ecommuter.model.Territories;
 import org.systemexception.ecommuter.model.Territory;
+
+import java.security.InvalidParameterException;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -24,8 +31,11 @@ public class TerritoriesTest {
 		territoryB = new Territory("IT", "456", "TEST");
 	}
 
+	@Rule
+	public final ExpectedException expectedException = ExpectedException.none();
+
 	@Test
-	public void add_territories() throws TerritoriesException {
+	public void add_territories() throws TerritoriesException, CsvParserException {
 		sut = new Territories();
 		sut.addTerritory(territoryA);
 		sut.addTerritory(territoryB);
@@ -33,7 +43,12 @@ public class TerritoriesTest {
 	}
 
 	@Test
-	public void add_duplicate_territory() throws TerritoriesException {
+	public void add_duplicate_territory() throws TerritoriesException, CsvParserException {
+
+		expectedException.expect(InvalidParameterException.class);
+		expectedException.expectMessage("isValidTerritory"  + Constants.LOG_OBJECT_SEPARATOR + "IT" +
+				Constants.LOG_ITEM_SEPARATOR + "123");
+
 		sut = new Territories();
 		sut.addTerritory(territoryA);
 		Territory badTerritory = new Territory("IT", "123", "TEST");

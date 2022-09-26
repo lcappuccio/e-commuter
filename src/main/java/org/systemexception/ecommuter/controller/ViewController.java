615ba287758e49f7be7762cc14d26680e12f5754
Leonardo Cappuccio <lcappuccio@users.noreply.github.com>
2022-09-26 02:44:25 +0200

    to getmapping

--- src/main/java/org/systemexception/ecommuter/controller/ViewController.java
+++ src/main/java/org/systemexception/ecommuter/controller/ViewController.java
@@ -1,15 +1,15 @@
 package org.systemexception.ecommuter.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.systemexception.ecommuter.enums.Endpoints;
 
 @Controller
 @RequestMapping(value = Endpoints.CONTEXT)
 public class ViewController {
 
-	@RequestMapping(method = RequestMethod.GET)
+	@GetMapping
 	public String viewAll() {
 		return "index";
 	}
